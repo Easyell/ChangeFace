0af578b8228d078965d2b2a3f17ed9aae425ad76
guoshencheng <648772021@qq.com>
2015-06-27 23:56:58 +0800

    test

--- Photo.js
+++ Photo.js
@@ -22,42 +22,61 @@ var canvas = document.getElementById("canvas"),
     dressImage = new Image(),
     uploadButton = document.getElementById("btnGetFile"),
     uploadPhoto;
+// add event handler
+var addEvent = (function () {
+    if (document.addEventListener) {
+        return function (el, type, fn) {
+            if (el && el.nodeName || el === window) {
+                el.addEventListener(type, fn, false);
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        };
+    } else {
+        return function (el, type, fn) {
+            if (el && el.nodeName || el === window) {
+                el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        };
+    }
+})();
 load();
 
 uploadButton.onclick = function loadImage() {
     var file = document.getElementById("fileDemo").files[0];
                     var fr = new FileReader();
                     fr.readAsDataURL(file);
-
                     fr.onload = function(fe){
                         var result = this.result;
                         uploadPhoto = new Image();
-                        var exif;
                         uploadPhoto.onload = function() {
-                            var orientation = exif ? exif.Orientation : 1;
-                            // 判断拍照设备持有方向调整照片角度
-                            switch(orientation) {
-                                case 3:
-                                    imgRotation = 180;
-                                    break;
-                                case 6:
-                                    imgRotation = 90;
-                                    break;
-                                case 8:
-                    imgRotation = 270;
-                    break;
-            }
-            ctx.save();
-            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-            ctx.drawImage(uploadPhoto, 0, 0);
-            ctx.globalAlpha = 0.8;
-            ctx.drawImage(dressImage, 0, 0);
-            ctx.restore();
-        };
+                            addEvent(document, 'touchmove', function(e){
+                                // Two finger gesture
+                                e.preventDefault();
+                                    var touches = e.changedTouches;
+                                    if(touches && touches.length == 1) {
+                                            requestAnimFrame(function() {
+                                                    move(touches[0]);
+                                            });
+                                    }
+                            });
+                            ctx.save();
+                            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+                            ctx.drawImage(uploadPhoto, 0, 0);
+                            ctx.globalAlpha = 0.8;
+                            ctx.drawImage(dressImage, 0, 0);
+                            ctx.restore();
+                    };
 
-        // 转换二进制数据
-        var base64 = result.replace(/^.*?,/,'');
-        uploadPhoto.src = result;
+                    // 转换二进制数据
+                    var base64 = result.replace(/^.*?,/,'');
+                    uploadPhoto.src = result;
     };
 }
 
@@ -117,22 +136,13 @@ function drawSquare(x, y, w, h){
     ctx.fillRect(x, y, w, h);   
 }
 
-// Add listeners
-document.addEventListener('touchmove', function(e){
-    e.preventDefault();
-    var touches = e.changedTouches;
-    if(touches && touches.length == 1) {
-        requestAnimFrame(function() {
-            move(touches[0]);
-        });
-    }
-
+addEvent(document, 'touchmove', function(e) {
     // Two finger gesture
     if(event.touches && event.touches.length === 2){
         requestAnimFrame(function(){
             rotate(e);
         });
     }
-}, false);
+});
 // Init 
 drawSquare(centerX - imgCenterX, centerY - imgCenterY, 100, 100);
